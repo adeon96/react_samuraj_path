c711058c6cda8453d4fe833411b27bf3163949a6
Roman Pomin <roman120696@gmail.com>
2020-04-23 13:43:56 +0300

    Lesson 19. Routing + added new components and links

--- src/App.js
+++ src/App.js
@@ -4,15 +4,29 @@ import './App.css';
 import Header from './components/Header/Header.jsx';
 import Sidebar from './components/Sidebar/Sidebar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+
+import { BrowserRouter, Route } from 'react-router-dom';
 
 
 const App = () => {
   return (
+    <BrowserRouter>
       <div className='app-wrapper'>
         <Header />
         <Sidebar />
-        <Profile />
-    </div>
+        <div className='app-wrapper-content'>
+          <Route path='/profile' component={Profile} />
+          <Route path='/dialogs' component={Dialogs} />
+          <Route path='/news' component={News} />
+          <Route path='/music' component={Music} />
+          <Route path='/settings' component={Settings} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import styles from './Sidebar.module.css';
 
-/**
- * let c1 = "item"
- * let c2 = "active"
- * 
- * let classes = c1 + " " + c2
- * let classesNew = `${c1} ${c2}`
- */
-
 const Sidebar = () => {
   return (
     <div className={styles.sidebar}>
       <nav>
         <ul>
-          <li><a className={styles.item} href="google.com">Profile</a></li>
-          <li><a href="google.com">Messages</a></li>
-          <li><a href="google.com">News</a></li>
-          <li><a href="google.com">Music</a></li>
-          <li><a href="google.com">Settings</a></li>
+          <li><a href="/profile">Profile</a></li>
+          <li><a href="/dialogs">Messages</a></li>
+          <li><a href="/news">News</a></li>
+          <li><a href="/music">Music</a></li>
+          <li><a href="/settings">Settings</a></li>
         </ul>
       </nav>
     </div>
